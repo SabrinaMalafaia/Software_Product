f9541a5937388c01bf9b55b623f2670dd1051e3a
Sabrina Malafaia <brina.sxm@hotmail.com>
2024-02-14 20:12:59 -0300

    Atualização de todos os arquivos html.

--- _rotas.py
+++ _rotas.py
@@ -18,9 +18,39 @@ def parceiro():
     return render_template("_parceiro.html")
 
 
+@app.route('/buscar_parceiro', methods=['GET'])
+def buscar_parceiro():
+    return render_template("_buscarParceiro.html")
+
+
+@app.route('/listar_parceiro', methods=['GET'])
+def listar_parceiro():
+    return render_template("_listarParceiros.html")
+
+
+@app.route('/cadastrar_parceiro', methods=['GET'])
+def cadastrar_parceiro():
+    return render_template("_cadastrarParceiro.html")
+
+
 @app.route('/grupo', methods=['GET'])
 def grupo():
     return render_template("_grupo.html")
+
+
+@app.route('/buscar_grupo', methods=['GET'])
+def buscar_grupo():
+    return render_template("_buscarGrupo.html")
+
+
+@app.route('/listar_grupo', methods=['GET'])
+def listar_grupo():
+    return render_template("_listarGrupos.html")
+
+
+@app.route('/cadastrar_grupo', methods=['GET'])
+def cadastrar_grupo():
+    return render_template("_cadastrarGrupo.html")
 
 
 @app.route('/contato', methods=['GET'])
